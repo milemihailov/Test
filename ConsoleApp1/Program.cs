79f32319fb51a45afae08440dee934f0cbcfdcfd
milemihailov <milemihailov5@gmail.com>
2023-05-20 13:28:10 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
